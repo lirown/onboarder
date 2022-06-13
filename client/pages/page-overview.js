b26f4ec276ab8d4f2c76dd7ab0676e90e3a88263
danirter <93980096+danirter@users.noreply.github.com>
2022-06-13 20:56:38 +0300

    wip

--- client/pages/page-overview.js
+++ client/pages/page-overview.js
@@ -14,14 +14,15 @@ export class PageOverview extends PageElement {
         .status-page {
           margin: 20px 0px;
           display: flex;
-          justify-content: center;
+          gap: 7px;
+          flex-direction: column;
+          padding: 10px;
         }
 
         .status-chart {
           display: flex;
-          justify-content: space-between;
-          width: 500px;
-          padding: 10px;
+          justify-content: center;
+          gap: 5px;
           border-radius: 8px;
         }
 
@@ -29,18 +30,31 @@ export class PageOverview extends PageElement {
           background-color: #1ee164;
           width: 2.5%;
           height: 80px;
-          border-radius: 300%;
+          border-radius: 300px;
+        }
+
+        .actions button {
+          border: none;
+          padding: 5px 10px;
+          border-radius: 5px;
+          background: #ffe9e9;
+          color: #db0000;
+          text-transform: uppercase;
         }
       </style>
 
       <section>
         <div>
-          <div class="title">API:</div>
+          <div class="title">Decisions API:</div>
           <div class="card status-page">
             <div class="status-chart">
               ${Array.from(Array(30).keys()).map(
                 (x) => html`<div class="status-bar"></div>`
               )}
+            </div>
+
+            <div class="actions">
+              <button>5 blocking issues detected</button>
             </div>
           </div>
         </div>
