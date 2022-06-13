d201bd15e1878405f2d9fa25a87eb4c8cc45d4da
danirter <93980096+danirter@users.noreply.github.com>
2022-06-13 19:55:07 +0300

    wip

--- client/pages/page-overview.js
+++ client/pages/page-overview.js
@@ -20,30 +20,23 @@ export class PageOverview extends PageElement {
         .status-chart {
           display: flex;
           justify-content: space-between;
-          background-color: #1f2e45;
           width: 500px;
           padding: 10px;
           border-radius: 8px;
         }
 
         .status-bar {
-          background-color: #00a6ff;
+          background-color: #1ee164;
           width: 2.5%;
           height: 80px;
-          border-radius: 3px;
-        }
-
-        .status-bar:hover {
-          transform: scale(1.2);
-          transition: transform 0.3s;
-          cursor: pointer;
+          border-radius: 300%;
         }
       </style>
 
       <section>
         <div>
           <div class="title">API:</div>
-          <div class="status-page">
+          <div class="card status-page">
             <div class="status-chart">
               ${Array.from(Array(30).keys()).map(
                 (x) => html`<div class="status-bar"></div>`
